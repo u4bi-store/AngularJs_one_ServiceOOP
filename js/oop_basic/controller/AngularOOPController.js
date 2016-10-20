c23d75c5d928a9f665495893ad8feba69d45bd40
u4bi <rootcode10@gmail.com>
2016-10-20 12:05:47 +0900

    InstanceService

--- js/oop_basic/controller/AngularOOPController.js
+++ js/oop_basic/controller/AngularOOPController.js
@@ -1,7 +1,21 @@
-app.controller('AngularOOPController', AngularOOPController);
+app.controller('AngularOOPController', AngularOOPController, AngularOOPInstanceService);
 
 function AngularOOPController($scope, AngularOOPService) {
+
     $scope.flag = AngularOOPService.flag(true);
     alert($scope.flag);
 
+    var weapon = [];
+    var bullet = [];
+
+    weapon[0] = new AngularOOPInstanceService.item(1, 'DE 본체', 'weapon', 5000);
+    weapon[1] = new AngularOOPInstanceService.item(2, 'Ak-47 본체', 'weapon', 15000);
+    weapon[2] = new AngularOOPInstanceService.item(3, 'M24 Sniper-gun 본체', 'weapon', 30000);
+
+    bullet[0] = new AngularOOPInstanceService.item(1, '50 AE 1세트', 'bullet', 500);
+    bullet[1] = new AngularOOPInstanceService.item(2, '7.62×39mm 1세트', 'weapon', 1500);
+    bullet[2] = new AngularOOPInstanceService.item(3, '.338 Lapua Magnum 1세트', 'weapon', 5000);
+    /* AngularOOPInstanceService.Item(id, name, type, price) */
+
+    alert(weapon[0].name);
 }
---
+++ js/oop_basic/service/AngularOOPInstanceService.js
@@ -0,0 +1,14 @@
+app.factory('AngularOOPInstanceService', AngularOOPInstanceService);
+
+function AngularOOPInstanceService(){
+    return{
+        'Item' : Item
+    }
+
+    function Item(item_id, name, type, price){
+        this.item_id = item_id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+}
